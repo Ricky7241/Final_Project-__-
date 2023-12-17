63368bf2b44818b0b605d9b0f13218fc9f21f855
Ricky7241 <130232732+Ricky7241@users.noreply.github.com>
2023-12-17 12:26:19 -0500

    Add files via upload

---
+++ Final Project/frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+import CreateUser from "./pages/CreateUser";
+import GroceriesApp from "./pages/GroceriesApp";
+import LoginUser from "./pages/LoginUser";
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import PrivateRoute from "./pages/PrivateRoute";
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+              {/* <Route path="/" element={<LoginUser /> } />
+         <Route path="/main" element={<GroceriesApp /> } />  
+         <PrivateRoute path="/main" element={<GroceriesApp />} />
+         <Route path="/create-user" element={<CreateUser /> } />
+      */}
+          <Route path="/login" element={<LoginUser />} />
+          <PrivateRoute path="/main" element={<GroceriesApp />} />
+          <Route path="/create-user" element={<CreateUser />} />
+
+        </Routes>
+      </BrowserRouter>  
+    </>
+  );
+}
+
+export default App;
